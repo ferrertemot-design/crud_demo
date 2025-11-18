8662666bbfe3a3c5c0370cb2a94e89bf684a0a5a
Temot69 <hed-ntferrer@smu.edu.ph>
2025-11-18 09:50:25 +0800

    Connected to MySQL Database

--- crud_demo/Form1.Designer.vb
+++ crud_demo/Form1.Designer.vb
@@ -22,10 +22,29 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ButtonConnect = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ButtonConnect
+        '
+        Me.ButtonConnect.Location = New System.Drawing.Point(12, 12)
+        Me.ButtonConnect.Name = "ButtonConnect"
+        Me.ButtonConnect.Size = New System.Drawing.Size(135, 58)
+        Me.ButtonConnect.TabIndex = 0
+        Me.ButtonConnect.Text = "Connect MySQL"
+        Me.ButtonConnect.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ButtonConnect)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents ButtonConnect As Button
 End Class
