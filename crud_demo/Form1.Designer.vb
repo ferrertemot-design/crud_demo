efc6893c102bf56bcaa1d25cf0553663822822bb
Temot69 <hed-ntferrer@smu.edu.ph>
2025-11-21 09:33:16 +0800

    Updated version

--- crud_demo/Form1.Designer.vb
+++ crud_demo/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.ButtonConnect = New System.Windows.Forms.Button()
         Me.TextBoxName = New System.Windows.Forms.TextBox()
@@ -32,12 +32,14 @@ Partial Class Form1
         Me.ButtonInsert = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.ButtonRead = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ButtonUpdate = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ButtonConnect
         '
-        Me.ButtonConnect.Location = New System.Drawing.Point(12, 12)
+        Me.ButtonConnect.Location = New System.Drawing.Point(335, 3)
         Me.ButtonConnect.Name = "ButtonConnect"
         Me.ButtonConnect.Size = New System.Drawing.Size(135, 58)
         Me.ButtonConnect.TabIndex = 0
@@ -46,7 +48,7 @@ Partial Class Form1
         '
         'TextBoxName
         '
-        Me.TextBoxName.Location = New System.Drawing.Point(66, 76)
+        Me.TextBoxName.Location = New System.Drawing.Point(298, 168)
         Me.TextBoxName.Name = "TextBoxName"
         Me.TextBoxName.Size = New System.Drawing.Size(216, 20)
         Me.TextBoxName.TabIndex = 4
@@ -54,7 +56,7 @@ Partial Class Form1
         'label1
         '
         Me.label1.AutoSize = True
-        Me.label1.Location = New System.Drawing.Point(11, 76)
+        Me.label1.Location = New System.Drawing.Point(243, 168)
         Me.label1.Name = "label1"
         Me.label1.Size = New System.Drawing.Size(38, 13)
         Me.label1.TabIndex = 3
@@ -62,7 +64,7 @@ Partial Class Form1
         '
         'TextBoxAge
         '
-        Me.TextBoxAge.Location = New System.Drawing.Point(66, 102)
+        Me.TextBoxAge.Location = New System.Drawing.Point(298, 194)
         Me.TextBoxAge.Name = "TextBoxAge"
         Me.TextBoxAge.Size = New System.Drawing.Size(216, 20)
         Me.TextBoxAge.TabIndex = 6
@@ -70,7 +72,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(11, 102)
+        Me.Label2.Location = New System.Drawing.Point(243, 194)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(29, 13)
         Me.Label2.TabIndex = 5
@@ -79,7 +81,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(11, 128)
+        Me.Label3.Location = New System.Drawing.Point(243, 220)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(35, 13)
         Me.Label3.TabIndex = 5
@@ -87,14 +89,14 @@ Partial Class Form1
         '
         'TextBoxEmail
         '
-        Me.TextBoxEmail.Location = New System.Drawing.Point(66, 128)
+        Me.TextBoxEmail.Location = New System.Drawing.Point(298, 220)
         Me.TextBoxEmail.Name = "TextBoxEmail"
         Me.TextBoxEmail.Size = New System.Drawing.Size(216, 20)
         Me.TextBoxEmail.TabIndex = 6
         '
         'ButtonInsert
         '
-        Me.ButtonInsert.Location = New System.Drawing.Point(12, 158)
+        Me.ButtonInsert.Location = New System.Drawing.Point(129, 58)
         Me.ButtonInsert.Name = "ButtonInsert"
         Me.ButtonInsert.Size = New System.Drawing.Size(270, 48)
         Me.ButtonInsert.TabIndex = 7
@@ -111,18 +113,38 @@ Partial Class Form1
         '
         'ButtonRead
         '
-        Me.ButtonRead.Location = New System.Drawing.Point(14, 214)
+        Me.ButtonRead.Location = New System.Drawing.Point(131, 114)
         Me.ButtonRead.Name = "ButtonRead"
         Me.ButtonRead.Size = New System.Drawing.Size(270, 48)
         Me.ButtonRead.TabIndex = 9
         Me.ButtonRead.Text = "READ (Retrieve Data)"
         Me.ButtonRead.UseVisualStyleBackColor = True
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(405, 58)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(270, 48)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Remove (Delete Data)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ButtonUpdate
+        '
+        Me.ButtonUpdate.Location = New System.Drawing.Point(407, 114)
+        Me.ButtonUpdate.Name = "ButtonUpdate"
+        Me.ButtonUpdate.Size = New System.Drawing.Size(270, 48)
+        Me.ButtonUpdate.TabIndex = 11
+        Me.ButtonUpdate.Text = "Update (Update Data)"
+        Me.ButtonUpdate.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ButtonUpdate)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.ButtonRead)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.ButtonInsert)
@@ -151,4 +173,6 @@ Partial Class Form1
     Friend WithEvents ButtonInsert As Button
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents ButtonRead As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ButtonUpdate As Button
 End Class
--- crud_demo/Form1.vb
+++ crud_demo/Form1.vb
@@ -1,4 +1,6 @@
-﻿Imports MySql.Data.MySqlClient
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports Google.Protobuf.WellKnownTypes
+Imports MySql.Data.MySqlClient
 Public Class Form1
     Dim conn As MySqlConnection
     Dim COMMAND As MySqlCommand
@@ -48,4 +50,71 @@ Public Class Form1
             MsgBox(ex.Message)
         End Try
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If DataGridView1.SelectedRows.Count > 0 Then
+            Dim selectedRow As DataGridViewRow = DataGridView1.SelectedRows(0)
+            Dim id As Integer = CInt(selectedRow.Cells("id").Value)
+
+            Dim query As String = "DELETE FROM students_tbl WHERE id = @id"
+            Try
+                Using conn As New MySqlConnection("server=localhost;userid=root;password=root;database=crud_demo_db;")
+                    conn.Open()
+                    Using cmd As New MySqlCommand(query, conn)
+                        cmd.Parameters.AddWithValue("@id", id)
+                        Dim rowsAffected As Integer = cmd.ExecuteNonQuery()
+                        If rowsAffected > 0 Then
+                            MessageBox.Show("Record Deleted Successfully")
+                        Else
+                            MessageBox.Show("No matching record found")
+                        End If
+                    End Using
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("Error: " & ex.Message)
+            End Try
+        Else
+            MessageBox.Show("Please select a row to delete")
+        End If
+    End Sub
+
+    Private Sub ButtonUpdate_Click(sender As Object, e As EventArgs) Handles ButtonUpdate.Click
+        DataGridView1.EndEdit()
+        If DataGridView1.CurrentCell Is Nothing Then
+            MessageBox.Show("Please select a row first!")
+            Return
+        End If
+
+        Dim rowIndex As Integer = DataGridView1.CurrentCell.RowIndex
+        Dim selectedRow As DataGridViewRow = DataGridView1.Rows(rowIndex)
+
+        ' Use the same column name as your DELETE method - "id" instead of index
+        Dim idValue = selectedRow.Cells("id").Value
+        Dim nameValue = selectedRow.Cells("Name").Value
+        Dim ageValue = selectedRow.Cells("Age").Value
+        Dim emailValue = selectedRow.Cells("Email").Value
+
+        If idValue IsNot Nothing AndAlso idValue.ToString().Trim() <> "" AndAlso Integer.TryParse(idValue.ToString(), Nothing) Then
+            ' Use "id" instead of "StudentID" to match your database column
+            Dim query As String = "UPDATE students_tbl SET Name=@Name, Age=@Age, Email=@Email WHERE id=@id"
+            Try
+                Using conn As New MySqlConnection("server=localhost;userid=root;password=root;database=crud_demo_db;")
+                    conn.Open()
+                    Using cmd As New MySqlCommand(query, conn)
+                        cmd.Parameters.AddWithValue("@Name", nameValue.ToString())
+                        cmd.Parameters.AddWithValue("@Age", CInt(ageValue))
+                        cmd.Parameters.AddWithValue("@Email", emailValue.ToString())
+                        cmd.Parameters.AddWithValue("@id", CInt(idValue))
+                        cmd.ExecuteNonQuery()
+                        MessageBox.Show("Record has been updated Successfully!")
+                    End Using
+                End Using
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        Else
+            MessageBox.Show("ID is invalid. Please select a valid row.")
+        End If
+    End Sub
+
 End Class
